4287cd9a422718ed5a8561645f3c7ccab96bc13a
Shailesh <shaileshkp001@gmail.com>
2022-06-11 18:08:08 +0530

    DB connect

--- server/index.js
+++ server/index.js
@@ -2,13 +2,44 @@ const express = require('express')
 
 const app = express()
 
+
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'mca2';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('students');
+  const findResult = await collection.find({
+    f_name: 'xyz2'
+  }).toArray();
+  console.log('findResult ', findResult)
+  // the following code examples can be pasted here...
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
 app.get('/', function (req, res) {
   res.send('Hello World1')
 })
 
 app.get('/api/v1/student', (req, res) => {
   res.status(200).json({
-    "name": "abc",
+    "name": "abcxy",
     "class": "xyz"
   })
 })
---
+++ server/promise.js
@@ -0,0 +1,35 @@
+/*
+// callback
+function addition(num1, num2, cb) {
+    let result = 0
+    setTimeout(() => {
+        result = num1+num2;
+        cb(result)
+    }, 1000);
+}
+
+let num1 = 2;
+let num2 = 3;
+let resultFun = (res) => {
+    console.log('result : ', res)
+}
+
+addition(num1, num2, resultFun);
+
+console.log('Function ended!')
+*/
+
+function addition(num1, num2) {
+    return new Promise((resolve, reject) => {
+        let result = 0
+        setTimeout(() => {
+            result = num1+num2;
+            resolve(result)
+        }, 1000);
+        // reject('result did not find')
+    })
+}
+
+addition(2, 3).then(result => {
+    console.log('result', result)
+}).catch(err => console.log(err))
